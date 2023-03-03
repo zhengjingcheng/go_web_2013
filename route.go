ec77fdc655abb7c54d61f488b422428cdeff9814
unknown <706710546@qq.com>
2023-03-03 10:10:57 +0800

    add context

---
+++ route.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"GO_WEB/framework"
+)
+
+func registerRouter(core *framework.Core) {
+	// core.Get("foo", framework.TimeoutHandler(FooControllerHandler, time.Second*1))
+	core.Get("foo", FooControllerHandler)
+}

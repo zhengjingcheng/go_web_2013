10d6348592bcecbbbefe4d489eb3537f84517496
unknown <706710546@qq.com>
2023-03-05 22:43:24 +0800

    middlerware

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"GO_WEB/framework"
+	middleware "GO_WEB/framework/middlerware"
 	"net/http"
 )
 
 func main() {
 	core := framework.NewCore()
+	core.Use(middleware.Recovery())
+	core.Use(middleware.Cost())
 	registerRouter(core)
 	server := &http.Server{
 		// 自定义的请求核心处理函数
